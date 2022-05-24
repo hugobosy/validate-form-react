a9f995690ef73518e356bf9ed1cc24672516a68e
HugoBosy <hkliszcz@gmail.com>
2022-05-24 14:25:59 +0200

    create form & two functions handleChange, handleForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
+    const [username, setUsername] = useState('')
+    const [useremail, setUseremail] = useState('')
+    const [userpass, setUserpass] = useState('')
 
-    </div>
-  );
+    const handleChange = e => {
+
+        switch (e.target.name) {
+            case "username":
+                setUsername(e.target.value);
+                break;
+            case "useremail":
+                setUseremail(e.target.value);
+                break;
+            case "userpass":
+                setUserpass(e.target.value);
+                break;
+            default:
+                console.log("Brak takiej wartości")
+        }
+    }
+
+    const handleForm = e => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={handleForm} noValidate>
+                <label htmlFor="user">
+                    <p>Twoje imię:</p>
+                    <input type="text" id="user" name="username" value={username} onChange={handleChange}/>
+                </label>
+                <label htmlFor="email">
+                    <p>Email:</p>
+                    <input type="email" id="email" name="useremail" value={useremail} onChange={handleChange}/>
+                </label>
+                <label htmlFor="password">
+                    <p>Hasło:</p>
+                    <input type="password" id="password" name="userpass" value={userpass} onChange={handleChange}/>
+                </label>
+                <button type="submit">Zapisz się!</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
